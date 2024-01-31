1e8ff37ad83a5f701f76c3e81dd8242595a64c5e
zakingleton <zakellisingleton@gmail.com>
2024-01-31 19:53:26 +0000

    completed all rules

--- src/game.test.js
+++ src/game.test.js
@@ -66,7 +66,7 @@ test('a live cell with one live neighbours dies', () => {
     ])
 })
 
-test('a live cell with two or three neighbours lives', () => {
+test('a live cell with two neighbours lives', () => {
     /*
         ...
         XXX
@@ -88,20 +88,88 @@ test('a live cell with two or three neighbours lives', () => {
         const nextCells = runNext(cells);
     
         expect(nextCells).toEqual([
-            [0,0,0],
             [0,1,0],
-            [0,0,0]
+            [0,1,0],
+            [0,1,0]
         ])
+})
+
+test('A live cell with three neighbours lives', () => {
+    const cells = [
+        [0,1,0],
+        [1,1,1],
+        [0,0,0]
+    ]
+
+    const nextCells = runNext(cells);
+
+    expect(nextCells).toEqual([
+        [1,1,1],
+        [1,1,1],
+        [0,1,0]
+    ])
+})
+
+test('Any live cell with more than three live neighbours dies', () => {
+    const cells = [
+        [1,1,0],
+        [1,1,1],
+        [0,0,0]
+    ]
+
+    const nextCells = runNext(cells);
+
+    expect(nextCells).toEqual([
+        [1,0,1],
+        [1,0,1],
+        [0,1,0]
+    ])
+})
+
+test('Any dead cell with exactly three live neighbours comes to life', () => {
+    const cells = [
+        [0,0,0],
+        [1,1,1],
+        [0,0,0]
+    ]
+
+    const nextCells = runNext(cells);
+
+    expect(nextCells).toEqual([
+        [0,1,0],
+        [0,1,0],
+        [0,1,0]
+    ])
+})
+
+test('Big Grid', () => {
+    const cells = [
+        [0,0,0,0],
+        [1,1,1,0],
+        [0,0,0,0],
+        [0,0,0,0]
+    ]
+
+    const nextCells = runNext(cells);
+
+    expect(nextCells).toEqual([
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,0,0,0]
+    ])
 })
 
 
 const runNext = (cells) => {
 
+    const gridDimension = cells.length;
+
     const isCellAlive = (row, column) => {
         if (row < 0) return false
-        if (row >= 3) return false
+        if (row >= gridDimension) return false
         if (column < 0) return false
-        if (column >= 3) return false
+        if (column >= gridDimension) return false
         
         return cells[row][column] === 1
     }
@@ -133,16 +201,29 @@ const runNext = (cells) => {
             count++
         }
         return count
+
+        // let count = 0;
+        // for(let i = -1; i <= 1; i++) {
+        //     for (let n = -1; n <= 1; n++) {
+        //         if ( i === 0 && n === 0) continue;
+
+        //         if (isCellAlive(row + i, column + n)) {
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
     }
 
-    const nextCells = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-    ]
+    const nextCells = [];
+    for (let i = 0; i < gridDimension; i++) {
+        const row = Array(gridDimension).fill(0, 0, gridDimension)
+        nextCells.push(row);
+    }
 
-    for (let row = 0; row < 3; row++) {
-        for (let column = 0; column < 3; column++) {
+    for (let row = 0; row < gridDimension; row++) {
+        for (let column = 0; column < gridDimension; column++) {
             const cell = cells[row][column]
             const liveNeighbours = countLiveNeighbours(row, column)
             if(cell === 1 && liveNeighbours < 2) {
@@ -151,10 +232,14 @@ const runNext = (cells) => {
             if (cell === 1 && liveNeighbours === 2) {
                 nextCells[row][column] = 1
             }
+            if (cell === 1 && liveNeighbours === 3) {
+                nextCells[row][column] = 1
+            }
+            if (cell === 0 && liveNeighbours === 3) {
+                nextCells[row][column] = 1
+            }
         }
     }
-
-    // https://meet.google.com/iyc-ouqz-bib
 
     return nextCells
 }
